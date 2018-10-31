a248f39cd945402c1d1201ba3e1c4b18c45c5463
wwoelmer <wwoelmer@vt.edu>
2018-10-31 13:22:07 -0400

    added in some weekly vectors to compare to the distribution of data

--- R Scripts/8_timestep.R
+++ R Scripts/8_timestep.R
@@ -8,12 +8,13 @@ library(ggplot2)
 data <- read.csv("./CTD/FCR_CTD_50_binned.csv")
 
 # first look at where the gaps are in each year
-### #2013 dates
-#make a dataframe that has every day in 2013
+
+#make a dataframe that has every day from 2013 to 2017
 years <- seq(as.Date("2013-01-01"), as.Date("2017-12-31"), by = "day")
 dates <- data.frame(matrix(ncol = 2, nrow = length(years)))
 dates[1] <- years
-colnames(dates) <- c("Date", "Y_N")
+colnames(dates) <- c("Date", "Y_N") 
+
 
 #make a dataframe consisting of dates where we have chla data (ie, no NAs)
 #first subset data to get rid of any days where we do not have chla data
@@ -31,6 +32,20 @@ abline(v =  seq(as.Date("2013-01-01"), as.Date("2017-12-31"), by = "week"))
 dates$Date <- as.Date(dates$Date)
 
 #the time period from Mar 1 to Oct 31 is chosen to represent the stratified period
+#vector of weeks from 2013-2017
+week13 <- data.frame(seq(as.Date("2013-03-01"), as.Date("2013-10-31"), by = "week"))
+week14 <- data.frame(seq(as.Date("2014-03-01"), as.Date("2014-10-31"), by = "week"))
+week15 <- data.frame(seq(as.Date("2015-03-01"), as.Date("2015-10-31"), by = "week"))
+week16 <- data.frame(seq(as.Date("2016-03-01"), as.Date("2016-10-31"), by = "week"))
+colnames(week13)[1] <- "Date"
+colnames(week14)[1] <- "Date"
+colnames(week15)[1] <- "Date"
+colnames(week16)[1] <- "Date"
+#ok, we can make a vector that consists of all the weeks in 2013-2017 that we want to have data for:
+weeks <- rbind(week13, week14, week15, week16)
+
+#now, we want to compare this list of dates to the list of dates that we have data, yes_dates
+plot(weeks)
 
 #look at 2013 alone
 thirteen <- dates[dates$Date  < "2013-12-31",]
@@ -46,7 +61,9 @@ y <- fourteen[fourteen$Date > "2014-03-01" & fourteen$Date < "2014-10-31",]
 yes.y <- y[y$Y_N==1,]
 plot(yes.y)
 abline(v =  seq(as.Date("2013-01-01"), as.Date("2017-12-31"), by = "week"))
+axis.Date(1, seq(as.Date("2013-01-01"), as.Date("2017-12-31"), by = "week"), labels = TRUE)
 title("timestep 2014")
+
 
 #look at 2015 alone
 fifteen <- dates[dates$Date  < "2015-12-31" & dates$Date > "2015-01-01",]
@@ -65,6 +82,13 @@ plot(yes.a)
 abline(v =  seq(as.Date("2013-01-01"), as.Date("2017-12-31"), by = "week"))
 title("timestep 2016")
 
+#look at 2017 alone
+seventeen <- dates[dates$Date  < "2017-12-31" & dates$Date > "2017-01-01",]
+b <- seventeen[seventeen$Date > "2017-03-01" & sixteen< "2017-10-31",]
+yes.b <- b[b$Y_N==1,]
+plot(yes.b)
+abline(v =  seq(as.Date("2013-01-01"), as.Date("2017-12-31"), by = "week"))
+title("timestep 2017")
 
 
 
