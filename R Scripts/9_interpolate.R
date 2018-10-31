afb85678154cf12b40dfa45f143f7f229e6621ed
wwoelmer <wwoelmer@vt.edu>
2018-10-31 16:34:08 -0400

    messed around working on interpolating

--- R Scripts/9_interpolate.R
+++ R Scripts/9_interpolate.R
@@ -1,22 +1,39 @@
+# script to interpolate days where chla, our response variable, does not have data in order to reach a weekly timestep
 
+install.packages("zoo")
+library(zoo)
 library(tidyverse)
 
 #master dataset
-data <- read.csv("./FCR_Master_2013_2017.csv")
+data <- read.csv("./CTD/FCR_CTD_50_binned.csv")
+#clean up and rearrange columns
 data$Date <- as.Date(data$Date)
-yes_dates <- unique(data$Date)
-#vector of weeks from 2013-2017
-week13 <- data.frame(seq(as.Date("2013-03-01"), as.Date("2013-10-31"), by = "week"))
-week14 <- data.frame(seq(as.Date("2014-03-01"), as.Date("2014-10-31"), by = "week"))
-week15 <- data.frame(seq(as.Date("2015-03-01"), as.Date("2015-10-31"), by = "week"))
-week16 <- data.frame(seq(as.Date("2016-03-01"), as.Date("2016-10-31"), by = "week"))
-colnames(week13)[1] <- "Date"
-colnames(week14)[1] <- "Date"
-colnames(week15)[1] <- "Date"
-colnames(week16)[1] <- "Date"
-#ok, we can make a vector that consists of all the weeks in 2013-2017 that we want to have data for:
-weeks <- rbind(week13, week14, week15, week16)
+data <- data %>% select(-Reservoir, -X) %>%
+        select(Date, everything())
+colnames(data)[3] <- "Depth"
 
-#now, we want to compare this list of dates to the list of dates that we have data, yes_dates
-plot(weeks)
+
+#	Create a new dataframe that consists of all the days that need to be interpolated that contains the same column headings
+#  THESE ARE THE DATES I WANT TO INTERPOLATE
+newdates <- as.Date(c( "2013-08-18", "2013-08-23", "2013-09-06", "2013-09-13", "2013-10-04", 
+            "2013-10-11", "2014-08-30", "2014-10-10", "2015-03-01", "2015-08-15", "2015-08-22", "2015-10-12", "2015-10-30", 
+            "2016-09-22", "2016-10-20", "2016-10-27"))
+add <- data.frame(matrix(nrow = length(newdates) , ncol = ncol(data)))
+colnames(add) <- colnames(data)
+add[1] <- newdates
+add$Site <- rep(50)
+
+# add in every depth for each date
+depths <- rep(c(0.1, 0.8, 1.6, 2.8, 3.8, 5.0, 5.2, 5.5, 5.8, 6.0, 6.2, 8.0, 9.0, 9.3), each = nrow(add))
+add <- cbind(depths, add)
+add$Depth <- depths
+add <- add%>% select(-depths)
+
+interp <- rbind(add, data)
+
+# try doing it by depth
+interp_0.1 <- interp[interp$Depth==0.1,]
+
+interp_0.1$Chla_interp <- na.approx(interp_0.1$Chla_ugL,  na.rm=FALSE, rule = 2, maxgap = 15)
+interp_0.1 <- mutate(interp_0.1, diff = Chla_interp - Chla_ugL)
 
